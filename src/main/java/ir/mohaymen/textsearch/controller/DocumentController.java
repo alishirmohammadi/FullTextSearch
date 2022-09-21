8b89e3284225d611cc01a4d8c1fd8fc8d0597ef0
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2022-09-21 11:22:02 +0430

    search rest controller

--- src/main/java/ir/mohaymen/textsearch/controller/DocumentController.java
+++ src/main/java/ir/mohaymen/textsearch/controller/DocumentController.java
@@ -3,7 +3,6 @@ package ir.mohaymen.textsearch.controller;
 import ir.mohaymen.textsearch.models.Document;
 import ir.mohaymen.textsearch.repository.DocumentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +16,6 @@ public class DocumentController {
 
     @GetMapping("/{id}")
     public Document getDocument(@PathVariable int id) {
-        System.out.println("salam");
-        Document document = documentRepository.findById(id);
-        System.out.println(document);
-        return document;
+        return documentRepository.findById(id);
     }
 }
--- src/main/java/ir/mohaymen/textsearch/controller/SearchController.java
+++ src/main/java/ir/mohaymen/textsearch/controller/SearchController.java
@@ -1,4 +1,30 @@
 package ir.mohaymen.textsearch.controller;
 
+import ir.mohaymen.textsearch.models.Document;
+import ir.mohaymen.textsearch.repository.DocumentRepository;
+import ir.mohaymen.textsearch.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/search")
 public class SearchController {
+    @Autowired
+    private SearchService searchService;
+    @Autowired
+    private DocumentRepository documentRepository;
+
+    @GetMapping("/{query}")
+    public List<Document> searchQuery(@PathVariable String query) {
+        return searchService.searchByQuery(query).stream().map(
+                documentRepository::findById
+        ).collect(Collectors.toList());
+    }
+
 }
